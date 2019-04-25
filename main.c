daf2a667be51f08c05d25d3332e0a62f8e91e33d
Nathan <hodgson_nathan@wheatoncollege.edu>
2019-04-25 15:17:03 -0400

    Process command line arguments

--- main.c
+++ main.c
@@ -12,8 +12,13 @@
 
 int main (int args, char** arg) {
 
+	char dictName[50], inputName[50], outputName[50];
+	strcpy(dictName, arg[1]);
+	strcpy(inputName, arg[2]);
+	strcpy(outputName, arg[3]);
+	int consumerThreads = atoi(arg[4]);
 
-
+	
 
 	return 0;
 }
