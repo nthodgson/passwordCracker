27b720b98ea9e1ecfad1f9e9e6fbd718f3b22e48
Nathan Hodgson <hodgson_nathan@wheatoncollege.edu>
2019-05-02 16:06:15 -0400

    Create threads

--- main.c
+++ main.c
@@ -9,24 +9,33 @@ int main (int args, char** arg) {
 	char hash[100], buffer[50], word[50];
 	strcpy(word, "oil");
 	strcpy(buffer, "oi18");
-	char dictName[50], inputName[50], outputName[50];
+	char inputName[50], outputName[50];
 	struct globalBuffer buffer_t;
 
 	if (args != 5) {
 		printf("Incorrect number of command line arguments. Terminating...\n");
 		exit(1);
 	}
+	if (pthread_mutex_init(&lock, NULL) != 0) {
+		printf("Mutex init has failed...\n");
+		exit(1);
+	}
+	for (int i=0; i<10000; i++) {
+		buffer_t.buf[i] = (char*)malloc(strlen(word) * sizeof(char));
+	}
 
-	strcpy(dictName, arg[1]);
+	strcpy(buffer_t.dictName, arg[1]);
 	strcpy(inputName, arg[2]);
 	strcpy(outputName, arg[3]);
-	int consumerThreads = atoi(arg[4]);
+	int numThreads = atoi(arg[4]);
 
-	// Thread creation
+	// Producer thread creation
 
-	readFile(dictName, &buffer_t);
+	pthread_t tid[numThreads+1];
+	pthread_create(&tid[0], NULL, readFile, (void*)&buffer_t);
+
 	sha_256_string(hash, buffer, strlen(buffer));
-	getWord(word, hash);
+	checkWord(word, hash);
 
 	return 0;
 }
