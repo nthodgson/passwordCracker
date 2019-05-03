5751dcda1eddf5d4db57fc9b0dcd9192b3942169
student <hodgson_nathan@wheatoncollege.edu>
2019-05-03 19:05:46 -0400

    Add writeFile

--- main.c
+++ main.c
@@ -6,26 +6,25 @@
 // Program: main.c
 
 int main (int args, char** arg) {
-	char buffer[50], word[50];
-	strcpy(word, "oil");
-	strcpy(buffer, "oi18");
+
 	char outputName[50], inputName[50];
 	struct globalBuffer buffer_t;
-	buffer_t.occupied = 1;
+	buffer_t.occupied = 0;
 	buffer_t.nextin = 0;
 	buffer_t.nextout = 0;
 	buffer_t.foundPass = false;
+	buffer_t.endOfFile = false;
 
 	if (args != 5) {
 		printf("Incorrect number of command line arguments. Terminating...\n");
 		exit(1);
 	}
 	if ((pthread_mutex_init(&lock, NULL) != 0) || (pthread_cond_init(&less, NULL) != 0) || (pthread_cond_init(&more, NULL) != 0)) {
-		printf("Pthread initialization has failed...\n");
+		printf("Thread initialization has failed...\n");
 		exit(1);
 	}
-	for (int i=0; i<10000; i++) {
-		buffer_t.buf[i] = (char*)malloc(strlen(word) * sizeof(char));
+	for (int i=0; i<10000; i++) { // Allocate memory for global buffer
+		buffer_t.buf[i] = (char*)malloc(50 * sizeof(char));
 	}
 
 	strcpy(buffer_t.dictName, arg[1]);
@@ -38,16 +37,28 @@ int main (int args, char** arg) {
 	// Producer and consumer thread creation
 
 	pthread_t tid[numThreads+1];
-	pthread_create(&tid[0], NULL, readFile, (void*)&buffer_t); // Producer
+	pthread_create(&tid[0], NULL, readFile, (void*)&buffer_t); // Create producer threads
 
-	for (int i=1; i<numThreads+1; i++)
-		pthread_create(&tid[i], NULL, getWord, (void*)&buffer_t); // Consumers
+	for (int i=1; i<numThreads+1; i++) // Create consumer threads
+		pthread_create(&tid[i], NULL, runConsumer, (void*)&buffer_t); 
 
-	for (int i=0; i<numThreads+1; i++) {
+	for (int i=0; i<numThreads+1; i++) { // Wait for threads to terminate and join
 		if (pthread_join(tid[i], NULL))
 			printf("ERROR ON TID: %ld\n", tid[i]);
 	}
 
+	if (buffer_t.foundPass) {
+		printf("Found the password!\n");
+		writeFile(&buffer_t, outputName);
+	}
+	else
+		printf("\nEnd of file reached. Password not found...\n");
+
+	for (int i=0; i<10000; i++) 
+		free(buffer_t.buf[i]);
+
+	pthread_cond_destroy(&more);
+	pthread_cond_destroy(&less);
 	pthread_mutex_destroy(&lock); // Destroy thread mutex
 	pthread_exit(NULL);
 
